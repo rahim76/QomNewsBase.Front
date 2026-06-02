f6c3a9ca602f1c463a75a7f23cd497a9d8668a7c
rahim76 <rahimx76@gmail.com>
2026-06-02 13:43:03 -0700

    fix sidebarRightAds

--- src/features/ads/widgets/SidebarRightAds/SidebarRightAds.vue
+++ src/features/ads/widgets/SidebarRightAds/SidebarRightAds.vue
@@ -1,24 +1,54 @@
 <template>
-  <div class="sidebar-ads">
-    <div v-if="isLoading">در حال بارگذاری...</div>
-    <div v-else-if="isError">{{ error.message }}</div>
-    <div v-else>
-      <div
-        v-for="ad in ads"
-        :key="ad.id"
-        class="ad-card"
-      >
+  <div v-if="!!ads" class="ads-box">
+    <span class="title">تبلیغات</span><br />
+
+    <swiper
+      :slides-per-view="1"
+      :space-between="20"
+      :loop="ads.length  > 3"
+      :autoplay="{ delay: 3000 }"
+      class="ads-swiper">
+
+      <swiper-slide v-for="ad in ads" :key="ad.id">
         <a :href="ad.targetUrl" target="_blank">
-          <img :src="ad.thumbnail" :alt="ad.title" />
-          <p class="ad-title">{{ ad.title }}</p>
+
+          <figure>
+          <span class="ad-badge">تبلیغات</span>
+          <img :src="ad.thumbnail" alt="" />
+        </figure>
+
         </a>
-      </div>
-    </div>
+      </swiper-slide>
+
+      <!-- Optional navigation -->
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination"></div>
+    </swiper>
   </div>
+
+  <div v-else>
+    <span>تبلیغی برای نمایش وجود ندارد.</span>
+  </div>
+
 </template>
 
 <script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import SwiperCore from 'swiper';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { useSidebarRightAds } from '../../composables/useSidebarRightAds';
 
+SwiperCore.use([Autoplay, Navigation, Pagination]);
+
 const { data: ads, isLoading, isError, error } = useSidebarRightAds()
+
+
+
 </script>
+
+<style scoped src="../../ui/styles/sidebarRightAds.css"></style>
